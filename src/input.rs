2d59441da7641c8912b9c755ae248adeee126e2d
kentakom1213 <kentakom1213@gmail.com>
2023-12-13 00:29:50 +0900

    [add] 隣接行列、隣接リスト形式への変換

--- src/input.rs
+++ src/input.rs
@@ -1,25 +1,33 @@
 use std::fs::read_to_string;
 
+/// グラフの隣接行列
+pub type AdjMatrix = Vec<Vec<bool>>;
+
+/// グラフの隣接リスト
+pub type AdjList = Vec<Vec<usize>>;
+
+/// 入力受け取り用構造体
 #[derive(Debug)]
 pub struct GraphInput {
-    pub v: usize,
-    pub e: usize,
+    pub v_size: usize,
+    pub e_size: usize,
     pub edges: Vec<(usize, usize)>,
 }
 
 impl GraphInput {
+    /// ファイル名を指定してグラフを読み込む
     pub fn input_from_ascii_file(filename: &str) -> Self {
         let file = read_to_string(filename).ok().unwrap();
 
-        let (mut v, mut e) = (0, 0);
+        let (mut v_size, mut e_size) = (0, 0);
         let mut edges = vec![];
 
         // 読み込み
         for line in file.lines() {
             if line.starts_with('p') {
                 if let [_, _, v_str, e_str] = line.split_whitespace().collect::<Vec<&str>>()[..] {
-                    v = v_str.parse::<usize>().unwrap();
-                    e = e_str.parse::<usize>().unwrap();
+                    v_size = v_str.parse::<usize>().unwrap();
+                    e_size = e_str.parse::<usize>().unwrap();
                 }
             }
             if line.starts_with('e') {
@@ -33,6 +41,33 @@ impl GraphInput {
             }
         }
 
-        Self { v, e, edges }
+        Self {
+            v_size,
+            e_size,
+            edges,
+        }
+    }
+
+    /// 隣接行列形式に変換
+    pub fn into_adjacent_matrix(&self) -> AdjMatrix {
+        self.edges.iter().fold(
+            vec![vec![false; self.v_size]; self.v_size],
+            |mut m, &(u, v)| {
+                m[u][v] = true;
+                m[v][u] = true;
+                m
+            },
+        )
+    }
+
+    /// 隣接リスト形式に変換
+    pub fn into_adjacent_list(&self) -> AdjList {
+        self.edges
+            .iter()
+            .fold(vec![vec![]; self.v_size], |mut g, &(u, v)| {
+                g[u].push(v);
+                g[v].push(u);
+                g
+            })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use maximal_clique_enumeration::input::GraphInput;
+use maximal_clique_enumeration::input::{AdjList, AdjMatrix, GraphInput};
 
 fn main() {
     let G = GraphInput::input_from_ascii_file("DIMACS_subset_ascii/brock200_2.clq");
 
-    println!("{:?}", G);
+    println!("{:?}", G.into_adjacent_list());
 }
